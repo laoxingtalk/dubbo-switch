6c04dd73d05e727cb985277a5e246eaa74ada5b8
xburning <xingzhiyu@foxmail.com>
2016-07-13 20:07:11 +0800

    去掉空行

--- src/main/java/com/github/xburning/dubboswitch/Application.java
+++ src/main/java/com/github/xburning/dubboswitch/Application.java
@@ -9,7 +9,6 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application  {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
 	}
